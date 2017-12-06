3bce9d7b18f103e53709d5cf277c34ccc55037c1
lilu <lilubz@163.com>
2017-12-06 16:19:03 +0800

    完成资产盘点

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -15,7 +15,7 @@ export class PaginationComponent implements OnInit {
   @Input() itemsPerPage; // 默认分页大小
   @Output() changePage: EventEmitter<number> = new EventEmitter<number>(); // 当前页号变化
   @Output() changeSize: EventEmitter<number> = new EventEmitter<number>(); // 分页大小改变
-  pageSizes = [1, 5, 10, 20, 40];
+  pageSizes = [ 5, 10, 20, 40];
   maxSize = 5; // 显示的分页链接数目
   _currentPage: number; // 当前页号
   constructor() { }
@@ -28,12 +28,10 @@ export class PaginationComponent implements OnInit {
     this.changePage.emit(event);
   }
   sizeChanged(event: any) {
-    // this.currentPage = 1;
     setTimeout(() => {
       this.currentPage = 1;
     }, 0);
     console.log('分页大小改变，当前大小:' + JSON.stringify(event));
-    console.log('当前页号:::::' + this.currentPage)
     this.itemsPerPage = event;
     this.changeSize.emit(event);
   }
